d9ce8cd66aa02863b7e5c1b293a21e857a389396
Paulo Clavijo Esteban <paucls@gmail.com>
2016-06-18 07:54:26 +0100

    First working version of the seed

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Kata Seed - TypeScript and Mocha
+================================
+
+This is a seed project for doing TDD code katas in TypeScript.
+
+Setup:
+   * `npm install`
+
+Run unit tests:
+   * `npm test`
---
+++ test/calculator.spec.ts
@@ -0,0 +1,21 @@
+/// <reference path="../typings/chai/chai.d.ts" />
+/// <reference path="../typings/mocha/mocha.d.ts" />
+import Calculator from '../app/calculator';
+import {expect} from 'chai';
+
+describe('Calculator', () => {
+    let calculator:Calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+
+    describe('add()', () => {
+
+        it('should add two numbers together', () => {
+            const result:number = calculator.add(2, 3);
+            expect(result).to.equal(5);
+        });
+
+    });
+});
